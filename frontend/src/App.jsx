e18ccada066f0d592401388caadd6df8e1f66954
gnos28 <gnos28@gmail.com>
2022-07-21 10:35:43 +0200

    login OK

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -6,8 +7,11 @@ import Realisations from "./pages/Realisations";
 import Technos from "./pages/Technos";
 import Team from "./pages/Team";
 import ContactUs from "./pages/ContactUs";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
 import Footer from "./components/Footer";
 import { PathContextProvider } from "./contexts/pathContext";
+import "react-toastify/dist/ReactToastify.css";
 import "./styles/App.scss";
 
 const App = () => {
@@ -23,10 +27,13 @@ const App = () => {
             <Route path="/tech" element={<Technos />} />
             <Route path="/team" element={<Team />} />
             <Route path="/contact" element={<ContactUs />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Admin />} />
           </Routes>
         </section>
         <Footer />
       </div>
+      <ToastContainer pauseOnHover position="bottom-right" autoClose={1000} />
     </PathContextProvider>
   );
 };
